c9aab913b6b4e9fdcbfef4ac22737957e551c787
Verónica Rebagliatte <rebagliatte@gmail.com>
2011-12-04 08:07:44 -0200

    moving highcharts dummy-data to view

--- gobtrans/parliament/static/javascripts/gobierno-transparente.js
+++ gobtrans/parliament/static/javascripts/gobierno-transparente.js
@@ -51,14 +51,7 @@ $(document).ready(function() {
         '#89A54E', 
         '#AA4643' 
       ],
-      series: [{
-         name: 'Asistencias',
-         data: [5, 3, 4, 7],
-         backgroundColor: 'green'
-      }, {
-         name: 'Inasistencias',
-         data: [3, 4, 4, 2]
-      }]
+      series: citations_data
    });
 
    // Attendance
@@ -92,22 +85,7 @@ $(document).ready(function() {
                this.x +': '+ this.y +'Â°C';
          }
       },
-      series: [{
-         name: 'faltca',
-         data: [8, 22, 10, 10, 48, 50, 38, 72, 2, 456, 4]
-      }, {
-         name: 'citaciones',
-         data: [62, 557, 1267, 1044, 1987, 1721, 1696, 1882, 914, 2317, 463]
-      }, {
-         name: 'pasajes',
-         data: [0, 0, 14, 12, 8, 0, 40, 32, 14, 48, 4]
-      }, {
-         name: 'licencias',
-         data: [4, 42, 126, 150, 285, 253, 342, 354, 164, 349, 75]
-      }, {
-         name: 'asist',
-         data: [46, 525, 1251, 1022, 1923, 1645, 1644, 1782, 908, 1843, 459]
-      }]
+      series: attendance_data
    });
 
    // Fancybox
